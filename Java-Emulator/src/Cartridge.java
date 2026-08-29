37a1b4dba2345a9eb7605d968340c2a1445fabb7
jorgehhh06 <jorgehazielherrera@gmail.com>
2026-08-28 23:13:04 -0600

    Add files via upload

--- Java-Emulator/src/Cartridge.java
+++ Java-Emulator/src/Cartridge.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 
 /*
  * Extrae la información del cartucho y se encarga de gestionar los datos de lectura/escritura, guardado y memory banking.
- * Ahora soporta MBC1, MBC2, MBC3 y MBC5.
+ * Ahora soporta MBC1, MBC3 y MBC5.
  */
 
 public class Cartridge {
@@ -59,68 +59,69 @@ public class Cartridge {
 
     // Publisher
     private static final String[] LIC_CODE = new String[0xA5];
+
     static {
-    [0x00] = "None",
-    [0x01] = "Nintendo R&D1",
-    [0x08] = "Capcom",
-    [0x13] = "Electronic Arts",
-    [0x18] = "Hudson Soft",
-    [0x19] = "b-ai",
-    [0x20] = "kss",
-    [0x22] = "pow",
-    [0x24] = "PCM Complete",
-    [0x25] = "san-x",
-    [0x28] = "Kemco Japan",
-    [0x29] = "seta",
-    [0x30] = "Viacom",
-    [0x31] = "Nintendo",
-    [0x32] = "Bandai",
-    [0x33] = "Ocean/Acclaim",
-    [0x34] = "Konami",
-    [0x35] = "Hector",
-    [0x37] = "Taito",
-    [0x38] = "Hudson",
-    [0x39] = "Banpresto",
-    [0x41] = "Ubi Soft",
-    [0x42] = "Atlus",
-    [0x44] = "Malibu",
-    [0x46] = "angel",
-    [0x47] = "Bullet-Proof",
-    [0x49] = "irem",
-    [0x50] = "Absolute",
-    [0x51] = "Acclaim",
-    [0x52] = "Activision",
-    [0x53] = "American sammy",
-    [0x54] = "Konami",
-    [0x55] = "Hi tech entertainment",
-    [0x56] = "LJN",
-    [0x57] = "Matchbox",
-    [0x58] = "Mattel",
-    [0x59] = "Milton Bradley",
-    [0x60] = "Titus",
-    [0x61] = "Virgin",
-    [0x64] = "LucasArts",
-    [0x67] = "Ocean",
-    [0x69] = "Electronic Arts",
-    [0x70] = "Infogrames",
-    [0x71] = "Interplay",
-    [0x72] = "Broderbund",
-    [0x73] = "sculptured",
-    [0x75] = "sci",
-    [0x78] = "THQ",
-    [0x79] = "Accolade",
-    [0x80] = "misawa",
-    [0x83] = "lozc",
-    [0x86] = "Tokuma Shoten Intermedia",
-    [0x87] = "Tsukuda Original",
-    [0x91] = "Chunsoft",
-    [0x92] = "Video system",
-    [0x93] = "Ocean/Acclaim",
-    [0x95] = "Varie",
-    [0x96] = "Yonezawa/s’pal",
-    [0x97] = "Kaneko",
-    [0x99] = "Pack in soft",
-    [0xA4] = "Konami (Yu-Gi-Oh!)"
+        LIC_CODE[0x00] = "None";
+        LIC_CODE[0x01] = "Nintendo R&D1";
+        LIC_CODE[0x08] = "Capcom";
+        LIC_CODE[0x13] = "Electronic Arts";
+        LIC_CODE[0x18] = "Hudson Soft";
+        LIC_CODE[0x19] = "b-ai";
+        LIC_CODE[0x20] = "kss";
+        LIC_CODE[0x22] = "pow";
+        LIC_CODE[0x24] = "PCM Complete";
+        LIC_CODE[0x25] = "san-x";
+        LIC_CODE[0x28] = "Kemco Japan";
+        LIC_CODE[0x29] = "seta";
+        LIC_CODE[0x30] = "Viacom";
+        LIC_CODE[0x31] = "Nintendo";
+        LIC_CODE[0x32] = "Bandai";
+        LIC_CODE[0x33] = "Ocean/Acclaim";
+        LIC_CODE[0x34] = "Konami";
+        LIC_CODE[0x35] = "Hector";
+        LIC_CODE[0x37] = "Taito";
+        LIC_CODE[0x38] = "Hudson";
+        LIC_CODE[0x39] = "Banpresto";
+        LIC_CODE[0x41] = "Ubi Soft";
+        LIC_CODE[0x42] = "Atlus";
+        LIC_CODE[0x44] = "Malibu";
+        LIC_CODE[0x46] = "angel";
+        LIC_CODE[0x47] = "Bullet-Proof";
+        LIC_CODE[0x49] = "irem";
+        LIC_CODE[0x50] = "Absolute";
+        LIC_CODE[0x51] = "Acclaim";
+        LIC_CODE[0x52] = "Activision";
+        LIC_CODE[0x53] = "American sammy";
+        LIC_CODE[0x54] = "Konami";
+        LIC_CODE[0x55] = "Hi tech entertainment";
+        LIC_CODE[0x56] = "LJN";
+        LIC_CODE[0x57] = "Matchbox";
+        LIC_CODE[0x58] = "Mattel";
+        LIC_CODE[0x59] = "Milton Bradley";
+        LIC_CODE[0x60] = "Titus";
+        LIC_CODE[0x61] = "Virgin";
+        LIC_CODE[0x64] = "LucasArts";
+        LIC_CODE[0x67] = "Ocean";
+        LIC_CODE[0x69] = "Electronic Arts";
+        LIC_CODE[0x70] = "Infogrames";
+        LIC_CODE[0x71] = "Interplay";
+        LIC_CODE[0x72] = "Broderbund";
+        LIC_CODE[0x73] = "sculptured";
+        LIC_CODE[0x75] = "sci";
+        LIC_CODE[0x78] = "THQ";
+        LIC_CODE[0x79] = "Accolade";
+        LIC_CODE[0x80] = "misawa";
+        LIC_CODE[0x83] = "lozc";
+        LIC_CODE[0x86] = "Tokuma Shoten Intermedia";
+        LIC_CODE[0x87] = "Tsukuda Original";
+        LIC_CODE[0x91] = "Chunsoft";
+        LIC_CODE[0x92] = "Video system";
+        LIC_CODE[0x93] = "Ocean/Acclaim";
+        LIC_CODE[0x95] = "Varie";
+        LIC_CODE[0x96] = "Yonezawa/s’pal";
+        LIC_CODE[0x97] = "Kaneko";
+        LIC_CODE[0x99] = "Pack in soft";
+        LIC_CODE[0xA4] = "Konami (Yu-Gi-Oh!)";
     }
 
     public String cart_lic_name() {
